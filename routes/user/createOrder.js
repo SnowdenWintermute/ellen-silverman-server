3ce80dd45217ab3a2399f869df197144d97ce6d9
unknown <michael.p.silverman@gmail.com>
2021-03-13 16:23:08 -0500

    multiple series for painting

--- routes/user/createOrder.js
+++ routes/user/createOrder.js
@@ -6,6 +6,7 @@ const { cloneDeep } = require('lodash')
 const sendOrderEmail = require('../../emails/user/sendOrderEmail')
 const sendAdminOrderNotificationEmail = require('../../emails/admin/sendAdminOrderNotificationEmail')
 const updateSeriesMetadata = require('../utils/series/updateSeriesMetadata')
+const Painting = require('../../models/painting')
 
 exports.createOrder = async (req, res) => {
   const { paymentIntent } = req.body.stripeResponse
@@ -25,7 +26,13 @@ exports.createOrder = async (req, res) => {
     sendAdminOrderNotificationEmail(newOrderForConfirmationEmail)
     await markPaintingsAsSoldOrDecrementStock(newOrder)
     const seriesIds = []
-    seriesIds.forEach(id => updateSeriesMetadata(id))
+    for (const painting of newOrder.paintings) {
+      const paintingFromDB = await Painting.findById(painting.painting).populate('seriesList')
+      paintingFromDB.seriesList.forEach(seriesId => {
+        if (!seriesIds.includes(seriesId)) seriesIds.push(seriesId)
+      })
+    }
+    updateSeriesMetadata(seriesIds)
     res.json({ ok: true })
   } catch (error) {
     console.log(error)
